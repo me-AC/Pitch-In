a9fdb2fa9e56e9444178d08dd5a1c2872fb14071
Abhinav <abhinavchaudhary2014@gmail.com>
2025-05-05 15:06:39 +0530

    Initial Commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { signIn } from '@/auth';
+import { signOut } from 'next-auth/react';
+import Image from 'next/image';
+
+export default function Home() {
+	return (
+		<>
+			<h1>Home</h1>
+		</>
+	);
+}
