e4faba8e14e447d1d598ce87355f414e8a158725
Abhinav <abhinavchaudhary2014@gmail.com>
2025-05-13 16:24:18 +0530

    feat: add playlist schema and integrate into Sanity types; update page to fetch editor picks

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,24 +1,37 @@
+import { Suspense } from 'react';
 import { client } from '@/sanity/lib/client';
-import { STARTUP_BY_ID_QUERY } from '@/sanity/lib/queries';
-import { Startup } from '@/sanity/types';
+import {
+	PLAYLIST_BY_SLUG_QUERY,
+	STARTUP_BY_ID_QUERY,
+} from '@/sanity/lib/queries';
 import { notFound } from 'next/navigation';
-import React, { Suspense } from 'react';
-import markdownit from 'markdown-it';
+import { formatDate } from '@/lib/utils';
 import Link from 'next/link';
 import Image from 'next/image';
-import { formatDate } from '@/lib/utils';
+
+import markdownit from 'markdown-it';
 import { Skeleton } from '@/components/ui/skeleton';
 import View from '@/components/View';
+import StartupCard, { StartupCardType } from '@/components/StartUpCard';
 
 const md = markdownit();
+
 export const experimental_ppr = true;
 
-const page = async ({ params }: { params: Promise<{ id: string }> }) => {
+const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
 	const id = (await params).id;
-	const post: Startup = await client.fetch(STARTUP_BY_ID_QUERY, { id });
-	const parsedContent = md.render(post?.pitch || '');
+
+	const [post, { select: editorPosts }] = await Promise.all([
+		client.fetch(STARTUP_BY_ID_QUERY, { id }),
+		client.fetch(PLAYLIST_BY_SLUG_QUERY, {
+			slug: 'editor-picks',
+		}),
+	]);
 
 	if (!post) return notFound();
+
+	const parsedContent = md.render(post?.pitch || '');
+
 	return (
 		<>
 			<section className="pink_container !min-h-[230px]">
@@ -73,6 +86,18 @@ const page = async ({ params }: { params: Promise<{ id: string }> }) => {
 
 				<hr className="divider" />
 
+				{editorPosts?.length > 0 && (
+					<div className="max-w-4xl mx-auto">
+						<p className="text-30-semibold">Editor Picks</p>
+
+						<ul className="mt-7 card_grid-sm">
+							{editorPosts.map((post: StartupCardType, i: number) => (
+								<StartupCard key={i} post={post} />
+							))}
+						</ul>
+					</div>
+				)}
+
 				<Suspense fallback={<Skeleton className="view_skeleton" />}>
 					<View id={id} />
 				</Suspense>
@@ -81,4 +106,4 @@ const page = async ({ params }: { params: Promise<{ id: string }> }) => {
 	);
 };
 
-export default page;
+export default Page;
